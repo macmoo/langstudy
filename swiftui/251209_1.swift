452d0ab5e3e15bc9240b818530b6f28385a1203d
kaku <hscrollhooli@gmail.com>
2025-12-11 01:28:36 +0900

    swift

---
+++ swiftui/251209_1.swift
@@ -0,0 +1,57 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        // ----------------------------
+        // Circle()
+        // Ellipse()
+        // Rectangle()
+        // RoundedRectangle(cornerRadius: 50)
+        // Capsule()
+        //    .foregroundColor(.blue)
+        //    .frame(width: 200, height: 300)
+        // ----------------------------
+        // Circle()
+        // .fill(Color.wakakusa)
+        // .foregroundColor(Color("Wakakusa"))
+        // .foregroundColor(.wakakusa)
+        // .padding(50)
+        // ----------------------------
+        // Ellipse()
+        // .foregroundColor(.orange)
+        // .frame(width: 200, height: 400)
+        // .rotationEffect(.degrees(45)) // 회전
+        // .border(Color.black, width: 1)
+        // .clipped()
+        // ----------------------------
+        // ZStack {
+        //     Ellipse()
+        //         .stroke(lineWidth: 4) // 선으로 그림
+        //         .foregroundColor(.pink)
+        //         .frame(width: 100, height: 300)
+        //     Ellipse()
+        //         .stroke(lineWidth: 4)
+        //         .foregroundColor(.purple)
+        //         .frame(width: 100, height: 300)
+        //         .rotationEffect(.degrees(30), anchor: .bottom)
+        //     Ellipse()
+        //         .stroke(lineWidth: 4)
+        //         .foregroundColor(.green)
+        //         .frame(width: 100, height: 300)
+        //         .rotationEffect(.degrees(-30), anchor: .bottom)
+        // }
+        // ----------------------------
+        ZStack {
+            Circle()
+                .foregroundColor(.green)
+                .frame(width: 100, height: 100)
+                .position(x:120, y:150)
+            Rectangle()
+                .foregroundColor(.orange)
+                .frame(width: 100, height: 100)
+                .position(x:300, y:350)
+        }
+        .border(Color.black, width: 1)
+        .edgesIgnoringSafeArea(.all) // safe area를 무시
+    }
+}
--- swiftui/251210_1.swift
+++ swiftui/251210_1.swift
@@ -2,56 +2,81 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+
         // ----------------------------
-        // Circle()
-        // Ellipse()
-        // Rectangle()
-        // RoundedRectangle(cornerRadius: 50)
-        // Capsule()
-        //    .foregroundColor(.blue)
-        //    .frame(width: 200, height: 300)
+        // Image("IMG_4291")
+        //     .resizable()
+        //     .aspectRatio(contentMode: .fill)
+        //     .frame(width:300, height:300)
+        //     .border(Color.red)
         // ----------------------------
-        // Circle()
-        // .fill(Color.wakakusa)
-        // .foregroundColor(Color("Wakakusa"))
-        // .foregroundColor(.wakakusa)
-        // .padding(50)
+        // Image("IMG_4291")
+        //     .resizable()
+        //     .aspectRatio(contentMode: .fill)
+        //     .frame(width: 300, height: 300)
+        //     // .clipShape(Circle())
+        //     .clipShape(RoundedRectangle(cornerRadius: 50))
+        //     .shadow(radius: 20)
         // ----------------------------
-        // Ellipse()
-        // .foregroundColor(.orange)
-        // .frame(width: 200, height: 400)
-        // .rotationEffect(.degrees(45)) // 회전
-        // .border(Color.black, width: 1)
-        // .clipped()
+        // Image("IMG_4291")
+        //     .resizable()
+        //     .aspectRatio(contentMode: .fill)
+        //     .frame(width: 300, height: 300)
+        //     .clipped()
+        //     .rotationEffect(.degrees(20), anchor: .center)
+        //     .border(Color.red)
         // ----------------------------
-        // ZStack {
-        //     Ellipse()
-        //         .stroke(lineWidth: 4) // 선으로 그림
-        //         .foregroundColor(.pink)
-        //         .frame(width: 100, height: 300)
-        //     Ellipse()
-        //         .stroke(lineWidth: 4)
-        //         .foregroundColor(.purple)
-        //         .frame(width: 100, height: 300)
-        //         .rotationEffect(.degrees(30), anchor: .bottom)
-        //     Ellipse()
-        //         .stroke(lineWidth: 4)
-        //         .foregroundColor(.green)
-        //         .frame(width: 100, height: 300)
-        //         .rotationEffect(.degrees(-30), anchor: .bottom)
-        // }
+        // Text("Hello World!")
+        //     .font(.body)
+        //     .frame(width: 150, height: 150)
+        //     .border(Color.pink, width: 10)
+        //     .cornerRadius(20)
+        // ----------------------------
+        // Text("Hello World")
+        //     .font(.largeTitle)
+        //     .padding(15)
+        //     .foregroundColor(.white)
+        //     .background(ShapeView())
+        //     .cornerRadius(50)
+        //     .frame(width: 150, height: 150)
+        // ----------------------------
+        // Text("春はあけぼの。やうやう白くなり行く、山ぎは少しあかりて、紫だちたる雲の細くたなびきたる。")
+        //     .fontWeight(.light)
+        //     .font(.title)
+        //     .frame(width: 250)
+        //     .rotation3DEffect(
+        //         .degrees(45), axis: (x: 0, y: 1, z: 0)
+        //     )
         // ----------------------------
         ZStack {
-            Circle()
-                .foregroundColor(.green)
-                .frame(width: 100, height: 100)
-                .position(x:120, y:150)
-            Rectangle()
-                .foregroundColor(.orange)
-                .frame(width: 100, height: 100)
-                .position(x:300, y:350)
+            Image("IMG_4291")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .offset(x: -70, y: 0)
+                .frame(width: 250, height: 400)
+                .clipped()
+
+            Text("ヨボー")
+                .fontWeight(.light)
+                .font(.title)
+                .foregroundColor(.white)
+                .padding()
+                .offset(x: 0, y: -5)
+                .frame(width: 250, height: 400)
         }
-        .border(Color.black, width: 1)
-        .edgesIgnoringSafeArea(.all) // safe area를 무시
+        .rotation3DEffect(.degrees(45), axis: (x: 1, y: 0, z: 0))
+
+        // ----------------------------
+    }
+}
+
+struct ShapeView: View {
+    var body: some View {
+        ZStack {
+            Rectangle().rotationEffect(.degrees(45))
+            Rectangle().rotationEffect(.degrees(-45))
+        }
+        .foregroundColor(.green)
+        .frame(width: 50, height: 150)
     }
 }
