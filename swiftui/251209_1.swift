07b349e2c8bbf37d215747fc69d202c3e8a36ebc
kaku <hscrollhooli@gmail.com>
2025-12-12 00:19:18 +0900

    swift

--- swiftui/251209_1.swift
+++ swiftui/251209_1.swift
@@ -7,39 +7,39 @@ struct ContentView: View {
         // Ellipse()
         // Rectangle()
         // RoundedRectangle(cornerRadius: 50)
-        // Capsule()
-        //    .foregroundColor(.blue)
-        //    .frame(width: 200, height: 300)
+        Capsule()
+           .foregroundColor(.blue)
+           .frame(width: 200, height: 300)
         // ----------------------------
-        // Circle()
-        // .fill(Color.wakakusa)
-        // .foregroundColor(Color("Wakakusa"))
-        // .foregroundColor(.wakakusa)
-        // .padding(50)
+        Circle()
+            .fill(Color.wakakusa)
+            .foregroundColor(Color("Wakakusa"))
+            .foregroundColor(.wakakusa)
+            .padding(50)
         // ----------------------------
-        // Ellipse()
-        // .foregroundColor(.orange)
-        // .frame(width: 200, height: 400)
-        // .rotationEffect(.degrees(45)) // 회전
-        // .border(Color.black, width: 1)
-        // .clipped()
+        Ellipse()
+            .foregroundColor(.orange)
+            .frame(width: 200, height: 400)
+            .rotationEffect(.degrees(45)) // 회전
+            .border(Color.black, width: 1)
+            .clipped()
         // ----------------------------
-        // ZStack {
-        //     Ellipse()
-        //         .stroke(lineWidth: 4) // 선으로 그림
-        //         .foregroundColor(.pink)
-        //         .frame(width: 100, height: 300)
-        //     Ellipse()
-        //         .stroke(lineWidth: 4)
-        //         .foregroundColor(.purple)
-        //         .frame(width: 100, height: 300)
-        //         .rotationEffect(.degrees(30), anchor: .bottom)
-        //     Ellipse()
-        //         .stroke(lineWidth: 4)
-        //         .foregroundColor(.green)
-        //         .frame(width: 100, height: 300)
-        //         .rotationEffect(.degrees(-30), anchor: .bottom)
-        // }
+        ZStack {
+            Ellipse()
+                .stroke(lineWidth: 4) // 선으로 그림
+                .foregroundColor(.pink)
+                .frame(width: 100, height: 300)
+            Ellipse()
+                .stroke(lineWidth: 4)
+                .foregroundColor(.purple)
+                .frame(width: 100, height: 300)
+                .rotationEffect(.degrees(30), anchor: .bottom)
+            Ellipse()
+                .stroke(lineWidth: 4)
+                .foregroundColor(.green)
+                .frame(width: 100, height: 300)
+                .rotationEffect(.degrees(-30), anchor: .bottom)
+        }
         // ----------------------------
         ZStack {
             Circle()
