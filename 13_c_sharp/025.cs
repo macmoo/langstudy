a218c8bc9525b7c10a75f230f979fdbe2bda1108
kaku <hscrollhooli@gmail.com>
2026-05-28 21:49:24 +0900

    csharp

---
+++ 13_c_sharp/025.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Numerics;
+
+// 부동소수점은 소수를 2진수의 분수로 표현하기 때문에
+// 10진수의 소수를 정확히 표현하지 못하는 경우가 있다.
+
+var d = 0.0;
+
+for (int i = 0; i < 100; i++)
+{
+    d += 0.01;
+}
+
+static bool DoubleEquals(double a, double b)
+{
+    var difference = a * 1.0E-14;
+    return Math.Abs(a - b) <= difference;
+}
+
+
+Console.WriteLine(d == 1.0 ? "same": "not same");
+
+Console.WriteLine(Double.Equals(d, 1.0)? "same" : "not same");
+
+Console.WriteLine(DoubleEquals(d, 1.0)? "same" : "not same");
+
+
+// not same
+// not same
+// same
---
+++ 13_c_sharp/026_random.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Security.Cryptography;
+
+var rnd = new Random(); // seed 없음
+// var rnd = new Random(5); // seed 있음 -> 같은 난수가 나옴
+
+// int
+for (int i = 0; i < 5; i++)
+{
+    var n = rnd.Next(0, 10); // 0 .. 9 의 난수
+    Console.Write($"{n}, ");
+}
+Console.WriteLine();
+
+// double 
+for (int i = 0; i < 5; i++)
+{
+    var n = rnd.NextDouble(); // 0.0 .. 0.9 의 난수
+    Console.Write($"{n}, ");
+}
+Console.WriteLine();
+
+// 보다 정도가 높은 난수
+for (int i = 0; i < 5; i++)
+{
+    var n = RandomNumberGenerator.GetInt32(0, 32);
+    Console.Write($"{n}, ");
+}
+Console.WriteLine();
