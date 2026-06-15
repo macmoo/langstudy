9188d5c9365bcc0f722573ad7f12fc7e171ffd4e
kaku <hscrollhooli@gmail.com>
2026-06-16 01:56:17 +0900

    csharp

---
+++ 13_c_sharp/161.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+var list = new[]
+{
+    new { Name = "武田", Attempts = 20, Success = 10 },
+    new { Name = "町田", Attempts = 17, Success = 11 },
+    new { Name = "文挾", Attempts = 22, Success = 16 },
+    new { Name = "高山", Attempts = 21, Success = 12 },
+    new { Name = "古澤", Attempts = 20, Success = 15 },
+};
+var top = list.Where(x => x.Attempts >= 20)
+               .Select(x => new { x.Name, Rate = (double)x.Success / x.Attempts })
+               .OrderByDescending(x => x.Rate)
+               .Take(2); // Top 2명만 취득
+foreach(var item in top)
+{
+    Console.WriteLine($"{item.Name} {item.Rate:0.00}");
+}
+// 古澤 0.75
+// 文挾 0.73
---
+++ 13_c_sharp/162.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using static System.Collections.Specialized.BitVector32;
+
+var persons = new List<Person>
+{
+        new Person { Name = "사카모토", SectionCode = 1 },
+        new Person { Name = "요시다", SectionCode = 2 },
+        new Person { Name = "이토이", SectionCode = 3 },
+        new Person { Name = "케이코", SectionCode = 1 },
+        new Person { Name = "도쿠가와", SectionCode = 2 },
+        new Person { Name = "오오쿠보", SectionCode = 4 },
+};
+var sections = new List<Section>
+{
+        new Section { Code = 1, SectionName = "영업부" },
+        new Section { Code = 2, SectionName = "총무부" },
+        new Section { Code = 3, SectionName = "제조부" },
+        new Section { Code = 0, SectionName = "기획부" },
+};
+
+var query = persons.GroupJoin(sections,
+                                p => p.SectionCode,
+                                s => s.Code,
+                                (p, ps) => new { p, ps })
+                   .SelectMany(x => x.ps.DefaultIfEmpty(),
+                                (x, s) => new { Name = x.p.Name, Section = s == null ? "(none)" : s.SectionName });
+foreach(var p in query)
+{
+    Console.WriteLine("{0}:{1}", p.Name, p.Section);
+}
+
+
+public class Person
+{
+    public string Name { get; set; }
+    public int SectionCode { get; set; }
+}
+
+public class Section
+{
+    public int Code { get; set; }
+    public string SectionName { get; set; }
+}
+
+// 사카모토:영업부
+// 요시다:총무부
+// 이토이:제조부
+// 케이코:영업부
+// 도쿠가와:총무부
+// 오오쿠보:(none)
