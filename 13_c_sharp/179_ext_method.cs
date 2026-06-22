a219b11d08abfd61e65ae83a3440235bd795f184
kaku <hscrollhooli@gmail.com>
2026-06-23 01:53:42 +0900

    csharp

---
+++ 13_c_sharp/179_ext_method.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+
+
+// 확장메소드
+// static class 클래스명
+// {
+//     public static 형 메소드명(this 형 인수, ...) { }
+// }
+//
+// 기존의 클래스, 구조체에 손대지 않고 새로운 메소드를 추가할 수 있다.
+// 확장메소드를 정의하는 클래스는 반드시 정적 클래스로 할 필요가 있다.
+// 당연히 확장메소드도 정적메소드.
+//
+//
+
+// 확장메소드 이용
+var name = "hello";
+Console.WriteLine(name.ToTitleCase());
+Console.WriteLine("world".ToTitleCase());
+Console.WriteLine("ｗｏｒｌｄ".ToTitleCase());
+
+var base64str = "C# Programming".ToBase64String();
+Console.WriteLine(base64str);
+var original = base64str.FromBase64String();
+Console.WriteLine(original);
+
+// 확장메소드 정의
+// string클래스를 확장
+static class StringExtensions
+{
+    public static string ToTitleCase(this string str)
+    {
+        var cultureInfo = Thread.CurrentThread.CurrentCulture;
+        var textInfo = cultureInfo.TextInfo;
+        return textInfo.ToTitleCase(str);
+    }
+
+    public static string ToBase64String(this string str, Encoding encoding = null)
+    {
+        var bytes = (encoding ?? Encoding.UTF32).GetBytes(str);
+        return Convert.ToBase64String(bytes);
+    }
+
+    public static string FromBase64String(this string base64String, Encoding encoding = null)
+    {
+        var bytes = Convert.FromBase64String(base64String);
+        return (encoding ?? Encoding.UTF32).GetString(bytes);
+    }
+}
+
+// Hello
+// World
+// Ｗｏｒｌｄ
+// QwAAACMAAAAgAAAAUAAAAHIAAABvAAAAZwAAAHIAAABhAAAAbQAAAG0AAABpAAAAbgAAAGcAAAA=
+// C# Programming
---
+++ 13_c_sharp/180_linq_ext_method.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+// 길이가 8이상인 요소가 포함되어 있는가
+var drinks = new[] {"wine", "sake", "beer", "whisky", "liqueur", "cocktail", "champagne"};
+var contains = drinks.Contains(x => x.Length >= 8);
+Console.WriteLine(contains);
+
+// 3으로 나누어 떨어지는 요소가 있는가
+var nums = new[] { 1, 3, 5, 7, 9, 11 };
+var contains2 = nums.Contains(x => x % 3 == 0);
+Console.WriteLine(contains2);
+
+// LINQ to Object를 확장하는 메소드
+// IEnumerable<T>인터페이스에 대해 확장메소드를 정의
+public static class EnumerableExtensions
+{
+    public static bool Contains<T>(this IEnumerable<T> source, Predicate<T> predicate)
+    {
+        foreach (var element in source)
+            if (predicate(element) == true)
+                return true;
+        return false;
+    }
+}
+
+// True
+// True
